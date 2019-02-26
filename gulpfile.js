349df344a137b4341efff511aa901442c9b30a22
aurelien-gosse <github@aurelien.email>
2019-02-26 08:55:59 +0100

    footer-responsive-v2

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,30 @@ let rename = require("gulp-rename");
 let minify = require('gulp-minify');
 let autoprefixer = require('gulp-autoprefixer');
 let browserSync = require('browser-sync');
-
+let wait = require('gulp-wait');
 let cache = require('gulp-cache');
 let imagemin = require('gulp-imagemin');
 let imageminPngquant = require('imagemin-pngquant');
 let imageminZopfli = require('imagemin-zopfli');
-let imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
+let imageminMozjpeg = require('imagemin-mozjpeg');
 let imageminGiflossy = require('imagemin-giflossy');
 
 
 // ***************
 // 2. Mes tâches * 
 // ***************
+
+// Moulinette HTML
 gulp.task('htmlification', function () {
   return gulp.src("./src/**/*.html")
+    .pipe(wait(200))
     .pipe(gulp.dest("./dist"));
 });
 
+// Moulinette SASS
 gulp.task('sassification', function () {
   return gulp.src('./src/css/**/*.scss')
+    .pipe(wait(200))
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -37,8 +42,10 @@ gulp.task('sassification', function () {
     .pipe(gulp.dest('./dist/css'));
 });
 
+// Moulinette JS
 gulp.task('jsification', function () {
   return gulp.src("./src/js/*.js")
+    .pipe(wait(200))
     .pipe(minify({
       ext: {
         min: '.min.js'
@@ -48,6 +55,8 @@ gulp.task('jsification', function () {
     .pipe(gulp.dest("./dist/js"));
 });
 
+
+// Browser Sync
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
@@ -56,14 +65,16 @@ gulp.task('browser-sync', function () {
   });
 });
 
+
+// Smushage
 gulp.task('compress', function () {
   return gulp.src(['./src/**/*.{gif,png,jpg,jpeg,svg}'])
     .pipe(cache(imagemin([
       //png
-      imageminPngquant({
+      /*imageminPngquant({
         speed: 1,
         quality: 98 //lossy settings
-      }),
+      }),*/
       imageminZopfli({
         more: true
         // iterations: 50 // very slow but more effective
@@ -101,9 +112,9 @@ gulp.task('compress', function () {
 // 3. Exécution des tâches *
 // *************************
 gulp.task('observe', gulp.parallel('browser-sync', 'compress', 'htmlification', 'sassification', 'jsification', function () {
+  gulp.watch("./src/**/*.html", gulp.series('htmlification'));
   gulp.watch('./src/css/**/*.scss', gulp.series('sassification'));
   gulp.watch('./src/js/*.js', gulp.series('jsification'));
-  gulp.watch("./src/**/*.html", gulp.series('htmlification'));
   gulp.watch('./dist/*.html').on('change', browserSync.reload);
   gulp.watch('./dist/css/*.css').on('change', browserSync.reload);
   gulp.watch('./dist/js/*.js').on('change', browserSync.reload);
